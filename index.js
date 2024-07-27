5af507a7fbdb990e62870e9dd98d4fdf73e143dc
AbdulQudri Abdullahi <abdulqudir2003@gmail.com>
2024-07-27 19:49:12 +0000

    Setting up

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+app.set("view engine", "ejs")
+app.set("views", __dirname + "/views")
+app.use(express.static(__dirname + "/public"))
+
 app.get("/", (req, res) => {
-	res.send("hello world!")
+	res.render("index", {title: "hello ejs!"})
 })
 
 app.listen(port, () => {
